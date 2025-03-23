3176746acec4430093d27dcf6c8941fbdfddb82d
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2025-03-23 14:44:02 +0700

    chore: integrate Swagger for API docs

--- src/api/user/user.routes.js
+++ src/api/user/user.routes.js
@@ -4,6 +4,27 @@ const { findUserById } = require('./user.services');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: User management endpoints
+ */
+
+/**
+ * @swagger
+ * /users/profile:
+ *   get:
+ *     summary: Get user profile
+ *     tags: [User]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Returns user profile (excluding password)
+ *       401:
+ *         description: Unauthorized
+ */
 router.get('/profile', isAuthenticated, async (req, res, next) => {
   try {
     const { userId } = req.payload;
