bed451897bc45f4698b76d1a808d6cef53e56b83
theunderdawg2005 <lqd.tik22.dinhcongthao@gmail.com>
2025-03-24 16:10:51 +0700

    feat: crud journey

--- src/api/journey/journey.routes.js
+++ src/api/journey/journey.routes.js
@@ -4,9 +4,7 @@ const journeyService = require('./journey.services')
 
 router.get('/', async (req, res, next) => {
     try {
-        res.json({
-            metadata: await journeyService.getAllJourneys()
-        })
+        res.json(await journeyService.getAllJourneys())
     } catch (error) {
         next(error)
     }
@@ -20,4 +18,30 @@ router.get('/:id', async (req, res, next) => {
     }
 })
 
+router.post('/', async (req, res, next) => {
+    try {
+        res.json(await journeyService.createJourney(req.body))
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.put('/update/:id', async (req, res, next) => {
+    try {
+        res.json(await journeyService.updateJourney(req.body, req.params.id))
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.delete('/delete/:id', async (req, res, next) => {
+    try {
+        res.json({
+            message: "Đã xóa journey",
+            data: await journeyService.deleteJourney(req.params.id)
+        })
+    } catch (error) {
+        next(error)
+    }
+})
 module.exports = router
--- src/api/journey/journey.services.js
+++ src/api/journey/journey.services.js
@@ -12,7 +12,7 @@ const getJourneyById = async (id) => {
 }
 
 const createJourney = async (journeyData) => {
-    const {user_id, name, description, destination} = journeyData
+    const {user_id, name, description, destinations} = journeyData
     return await db.journeys.create({
         data: {
             user_id: parseInt(user_id),
@@ -24,8 +24,29 @@ const createJourney = async (journeyData) => {
     })
 }
 
+const updateJourney = async (updateData, id) => {
+    const {user_id, name, description, destinations} = updateData
+    return await db.journeys.update({
+        where: {id: parseInt(id)},
+        data: {
+            user_id: parseInt(user_id),
+            name,
+            description,
+            destinations
+        }
+    })
+}
+
+const deleteJourney = async (id) => {
+    return await db.journeys.delete({
+        where: {id: parseInt(id)}
+    })
+}
+
 module.exports = {
     getAllJourneys,
     getJourneyById,
-    createJourney
+    createJourney,
+    updateJourney,
+    deleteJourney
 }
