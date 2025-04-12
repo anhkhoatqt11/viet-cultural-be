e8fb888cbcc732d6ece8cd4d527b78fdb139fbda
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2025-04-12 21:34:55 +0700

    fea: update schema and game fnction

--- src/api/game/game.services.js
+++ src/api/game/game.services.js
@@ -1,14 +1,39 @@
 const { db } = require('../../utils/db');
 
 
-function getGameDataByRegionAndType(regionId, gameType) {
-    return db.game.findMany({
-        where: {
-            regionId: regionId,
-            gameType: gameType,
-        },
-    });
+async function getGameDataByRegionAndType(regionId, gameType) {
+    try {
+        const gameData = await db.game_type.findUnique({
+            where: { id: gameType },
+            include: {
+                puzzle_game: {
+                    where: { regionid: regionId },
+                    include: {
+                        puzzle_pieces: true,
+                    },
+                },
+                quiz_game: {
+                    where: { regionid: regionId },
+                    include: {
+                        quiz_game_qa: true,
+                    },
+                },
+                treasure_game: {
+                    where: { regionid: regionId },
+                    include: {
+                        treasure_connection_cards: true,
+                    },
+                },
+            },
+        });
+
+        return gameData;
+    } catch (error) {
+        console.error("Error fetching game data:", error);
+        throw new Error("Failed to fetch game data");
+    }
 }
+
 
 module.exports = {
     getGameDataByRegionAndType,
