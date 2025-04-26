028be696343c87426cd4c722614c820bc688403c
Trương Anh Khoa <anhkhoatqt11@gmail.com>
2025-04-27 02:03:36 +0700

    fix: fix logic bug in cors

--- src/app.js
+++ src/app.js
@@ -31,13 +31,16 @@ app.use(
 
 // CORS configuration
 const corsOptions = {
-  origin: ['http://localhost:5173' || 'https://cutural-vie.vercel.app'],
+  origin: [
+    'http://localhost:5173',
+    'https://cutural-vie.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: [
-    'Content-Type', 
-    'Authorization', 
-    'Origin', 
-    'X-Requested-With', 
+    'Content-Type',
+    'Authorization',
+    'Origin',
+    'X-Requested-With',
     'Accept'
   ],
   credentials: true,
